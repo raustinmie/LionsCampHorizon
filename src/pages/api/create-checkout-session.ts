404b6dd3597976b8701d20459899266ba96ab05a
Austin Miedema <r.austin.mie@gmail.com>
2025-12-17 12:14:13 -0800

    fix build

--- src/pages/api/create-checkout-session.ts
+++ src/pages/api/create-checkout-session.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-	apiVersion: "2025-11-17.clover",
+	apiVersion: "2025-12-15.clover",
 });
 
 export default async function handler(
